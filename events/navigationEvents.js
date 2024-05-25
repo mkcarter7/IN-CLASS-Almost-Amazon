601c1320211616c1ad231ccb578cdeb6058614bf
Melissa Carter <mkd.princess@gmail.com>
2024-05-25 07:07:27 -0500

    mc fixed event listender for authors

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -1,3 +1,4 @@
+import { getAuthors } from '../api/authorData';
 import { getBooks } from '../api/bookData';
 import { showBooks } from '../pages/books';
 import { signOut } from '../utils/auth';
@@ -23,7 +24,7 @@ const navigationEvents = () => {
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
   document.querySelector('#authors').addEventListener('click', () => {
-    console.warn('CLICKED AUTHORS');
+    getAuthors().then(showBooks);
   });
 
   // STRETCH: SEARCH
