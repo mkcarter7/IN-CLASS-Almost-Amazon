6ead5456bc6c6866d84a485f9c4bcc47d071edcd
Melissa Carter <mkd.princess@gmail.com>
2024-05-30 20:08:58 -0500

    added button names and author

--- events/navigationEvents.js
+++ events/navigationEvents.js
@@ -23,25 +23,23 @@ const navigationEvents = () => {
   // FIXME: STUDENTS Create an event listener for the Authors
   // 1. When a user clicks the authors link, make a call to firebase to get all authors
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
-  // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
-  document.querySelector('#all-authors').addEventListener('click', () => {
+  document.querySelector('#authors').addEventListener('click', () => {
     getAuthors().then(showAuthors);
   });
+  // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
 
   // STRETCH: SEARCH
-  document.querySelector('#search').addEventListener('keyup', (e) => {
-    const searchValue = document.querySelector('#search').value.toLowerCase();
-    console.warn(searchValue);
-
-    // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
-    if (e.keyCode === 13) {
-      // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
-      // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
-      // OTHERWISE SHOW THE STORE
-
-      document.querySelector('#search').value = '';
-    }
-  });
+//   document.querySelector('#search').addEventListener('keyup', (e) => {
+//     const searchValue = document.querySelector('#search').value.toLowerCase();
+//     console.warn(searchValue);
+//     // WHEN THE USER PRESSES ENTER, MAKE THE API CALL AND CLEAR THE INPUT
+//     if (e.keyCode === 13) {
+//       // MAKE A CALL TO THE API TO FILTER ON THE BOOKS
+//       // IF THE SEARCH DOESN'T RETURN ANYTHING, SHOW THE EMPTY STORE
+//       // OTHERWISE SHOW THE STORE
+//       document.querySelector('#search').value = '';
+//     }
+//   });
 };
 
 export default navigationEvents;
