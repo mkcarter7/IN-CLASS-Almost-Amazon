f1f366627e725485c4fc878a90092b4a78e67410
Melissa Carter <mkd.princess@gmail.com>
2024-06-01 07:51:09 -0500

    added user to start app

--- utils/startApp.js
+++ utils/startApp.js
@@ -9,15 +9,15 @@ import navigationEvents from '../events/navigationEvents';
 import { showBooks } from '../pages/books';
 
 const startApp = (_user) => {
-  domBuilder(); // BUILD THE DOM
-  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
-  formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
+  domBuilder(_user); // BUILD THE DOM
+  domEvents(_user); // ADD THE EVENT LISTENTERS TO THE DOM
+  formEvents(_user); // ADD FORM EVENT LISTENTERS TO THE DOM
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
 
   // TODO: Put all books on the DOM on App load
-  getBooks().then((books) => showBooks(books));
+  getBooks(_user.uid).then((books) => showBooks(books));
 };
 
 export default startApp;
